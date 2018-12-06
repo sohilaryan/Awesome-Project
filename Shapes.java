728eb4a1305b602c31ef50a284bcee66da488c5a
sohilaryan <sohilaryan@yahoo.com>
2018-12-05 22:31:12 -0500

    adding shapes file

---
+++ Shapes.java
@@ -0,0 +1,111 @@
+package com.DrawingShapes;
+
+public class SOPwithShapes {
+
+	public static void main(String[] args) {
+
+		System.out.println("          /|");
+		System.out.println("         / |");
+		System.out.println("        /  |");
+		System.out.println("       /   |");
+		System.out.println("      /    |");
+		System.out.println("     /     |");
+		System.out.println("    /      |");
+		System.out.println("   /       |");
+		System.out.println("  /________|");
+		
+		
+		System.out.println(" _____________");
+		System.out.println(" |           |");
+		System.out.println(" |     S     |");
+		System.out.println(" |     Y     |");
+		System.out.println(" |     N     |");
+		System.out.println(" |     T     |");
+		System.out.println(" |     A     |");
+		System.out.println(" |     X     |");
+		System.out.println(" |___________|");
+		
+		
+		System.out.println("          /|\\");
+		System.out.println("         / | \\");
+		System.out.println("        /  |  \\");
+		System.out.println("       /   |   \\");
+		System.out.println("      /    |    \\");
+		System.out.println("     /     |     \\");
+		System.out.println("    /      |      \\");
+		System.out.println("   / SYNTAX|TECHGS \\");
+		System.out.println("  /________|________\\");
+		
+		
+		
+		System.out.println("       | | | |___| | | |     ");
+		System.out.println("      <|=|=|=|=|=|=|=|=|>    ");
+		System.out.println("       | | | | | | | | |     ");
+		System.out.println("               |   		     ");
+		System.out.println("              (|)   		 ");
+		System.out.println("               |   		  	 ");
+		System.out.println("               |   		     ");
+		System.out.println("               |   		     ");
+		System.out.println("               |   		     ");
+		System.out.println("               |   		     ");
+		
+		
+		
+
+		System.out.println("             /\\     	    ");
+		System.out.println("            /  \\     	    ");
+		System.out.println("     ======/    \\======    ");
+		System.out.println("      \\             //     ");
+		System.out.println("       \\           //	    ");
+		System.out.println("       //           \\	    ");
+		System.out.println("      //             \\     ");
+		System.out.println("     ======\\    /======    ");
+		System.out.println("            \\  /  	        ");
+		System.out.println("             \\/  	        ");
+		
+		
+		
+		System.out.println("             /\\     	    ");
+		System.out.println("            /  \\     	    ");
+		System.out.println("           /    \\     	    ");
+		System.out.println("          /      \\     	");
+		System.out.println("         /        \\     	");
+		System.out.println("        /          \\     	");
+		System.out.println("        \\          /     	");
+		System.out.println("         \\        /     	");
+		System.out.println("          \\      /      	");
+		System.out.println("           \\    /     	    ");
+		System.out.println("            \\  /     	    ");
+		System.out.println("             \\/     	    ");
+		
+		System.out.println("             ____                        ");
+		System.out.println("            /    |                       ");
+		System.out.println("           /~O   |                       ");
+		System.out.println("    ,---------------------------,        ");
+		System.out.println("   {|  TOYOTA  ~ |  IN 1975 LoL |        ");
+		System.out.println("    `---@-----------------@-----!        ");
+		
+		
+		
+		System.out.println("                 ,==================,                                  ");
+		System.out.println("                /         ||        |                                  ");
+		System.out.println("               /          ||        |	                               ");
+		System.out.println("              / ,(~)      ||        |~~~~~~~~~~~~~~~~~~~~~~~~~~~,      ");
+		System.out.println("   ,^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^:                          |      ");
+		System.out.println("   |         :         ~~  :      ~~ :    SYNTAX TECHNOLOGIES   |      ");
+		System.out.println("  {|         :             :         :                          |      ");
+		System.out.println("  {[   , ~,  :        	   :         :              , ^ ,      [|      ");
+		System.out.println(" [****( @  )***************************************(  @  )*******      ");
+		System.out.println("       ~  ~                                          ~  ~              ");
+		System.out.println("         `                                            `                ");
+		
+		
+		System.out.println("     =========  \\   //	  ||\\    ||  =========   ");
+		System.out.println("    ||           \\ //    || \\   ||      ||      ");
+		System.out.println("     =========    |||  	  ||  \\  ||     ");
+		System.out.println("             ||   |||     ||   \\ ||     ");
+		System.out.println("     =========    |||	  ||    \\||     ");
+		
+	}
+
+}
